bc81b36db8045422eb44eaf3a8288b1b82479bf2
Arzije Ziberovska <arzije.ziberovska@gmail.com>
2023-11-23 16:59:26 +0100

    Second commit

--- src/main/java/org/arzije/ziberovska/Main.java
+++ src/main/java/org/arzije/ziberovska/Main.java
@@ -1,7 +1,12 @@
 package org.arzije.ziberovska;
 
+import org.arzije.ziberovska.main.Prime;
+
 public class Main {
     public static void main(String[] args) {
+
+        Prime primeNumbers = new Prime();
+        System.out.println(primeNumbers.getPrimes());
 
     }
 }
